d5b7dd27e1fedfc03eea44987cdd7feb7bf1968f
Robert Quan <rob.quan@gmail.com>
2018-02-01 13:18:45 -0700

    took out cookies to try to fix errors

--- routes/users.js
+++ routes/users.js
@@ -4,34 +4,34 @@ const knex = require('../knex')
 const bodyParser = require('body-parser')
 const jwt = require('jsonwebtoken')
 const cookieParser = require('cookie-parser')
-const secret = process.env.SECRET || 'A4e2n84E0OpF3wW21'
+const secret = process.env.SECRET
 
 const getUsers = function(req, res, next) {
-  var cookie = req.headers['cooker']
-  var jwot = cookie.split(';')[1].split('=')[1]
-  var decoded = jwt.verify(jwot, secret, function(err, decoded) {
-    if(err){
-      next(err)
-    }else{
-      return decoded
-    }
-  })
-  if(!decoded.admin)res.sendStatus(403)
+  // var cookie = req.headers['cooker']
+  // var jwot = cookie.split(';')[1].split('=')[1]
+  // var decoded = jwt.verify(jwot, secret, function(err, decoded) {
+  //   if(err){
+  //     next(err)
+  //   }else{
+  //     return decoded
+  //   }
+  // })
+  // if(!decoded.admin)res.sendStatus(403)
   knex('users').then(data=>{res.status(200).send({data})})
   .catch(err=>{next(err)})
 }
 
 const deleteUser = function(req,res,next){
   var cookie = req.headers['cooker']
-  var jwot = cookie.split(';')[1].split('=')[1]
-  var decoded = jwt.verify(jwot, secret, function(err, decoded) {
-    if(err){
-      next(err)
-    }else{
-      return decoded
-    }
-  })
-  if(!decoded.admin)res.sendStatus(403)
+  // var jwot = cookie.split(';')[1].split('=')[1]
+  // var decoded = jwt.verify(jwot, secret, function(err, decoded) {
+  //   if(err){
+  //     next(err)
+  //   }else{
+  //     return decoded
+  //   }
+  // })
+  // if(!decoded.admin)res.sendStatus(403)
   knex('users').where({id : req.params.id}).del()
   .then(data=>{res.sendStatus(200)})
   .catch(err=>{next(err)})
